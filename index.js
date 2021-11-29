6684c6b62b77587f6d08661d88bf7282c5f40e4b
Sunil <snilavarath@gmail.com>
2021-11-29 18:00:27 -0500

    commit tree cbbf4dfc4abb3326adb033703996c9989cbfdaa7

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+// function superBowlWin (array){
+//     let wins = undefined
+//     for (let item of array){
+//         if (item.result === 'W'){
+//             wins = item.year
+//         }
+//     }
+//     return wins
+// }
+
+// superbowlWin(record)
+
+function superbowlWin(record){
+    const desiredOutcome = record.find(element => element.result ==='W')
+    return desiredOutcome ? desiredOutcome.year : undefined
+}
+
 
